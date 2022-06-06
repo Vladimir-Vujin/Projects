22fca31dc3484174fbbaa41402e4b8a9586f4f55
Vladimir Vujin <92719153+Vladimir-Vujin@users.noreply.github.com>
2022-06-06 13:07:23 +0200

    Update CaesarCipherClass.java

--- src/main/java/CaesarCipherClass.java
+++ src/main/java/CaesarCipherClass.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 public class CaesarCipherClass {
 
     private String textForEncoding;
+    private String keyForEncoding;
     private final ArrayList<Character> alphabetLetters = new ArrayList<Character>(Arrays.asList('A', 'B', 'C', 'D',
             'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
             'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
@@ -11,8 +12,9 @@ public class CaesarCipherClass {
     public CaesarCipherClass() {
     }
 
-    public CaesarCipherClass(String textForEncoding) {
+    public CaesarCipherClass(String textForEncoding, String keyForEncode) {
         this.textForEncoding = textForEncoding;
+        this.keyForEncoding = keyForEncode;
     }
 
     public String getTextForEncoding() {
@@ -23,22 +25,42 @@ public class CaesarCipherClass {
         this.textForEncoding = textForEncoding;
     }
 
-    public String encodeText() {
-        //System.out.println(textForEncoding);
+    public String getKeyForEncoding() {
+		return keyForEncoding;
+	}
+
+	public void setKeyForEncoding(String keyForEncoding) {
+		this.keyForEncoding = keyForEncoding;
+	}
+
+	public String encodeText() {
         char[] textForEncodingLetters = textForEncoding.toCharArray();
+        int key;
         String encryptedText = "";
         int encryptLetterIndex = 0;
+        
+        if(keyForEncoding.contains("-")) {
+        	key =  Integer.parseInt(keyForEncoding.substring(1, keyForEncoding.length()));
+        	key *= -1;
+        } else {
+        	key =  Integer.parseInt(keyForEncoding);
+        }
+        if(key >= 26 || key <= -26) {
+        	key = key % 26;
+        } 
 
         for (int i = 0; i < textForEncodingLetters.length; i++) {
             if (textForEncodingLetters[i] == ' ') {
                 encryptedText = encryptedText + " ";
-            } else {
-                encryptLetterIndex = alphabetLetters.indexOf(textForEncodingLetters[i]) - 3;
-                if (encryptLetterIndex < 0)
-                    encryptLetterIndex += alphabetLetters.size();
+            }  else {
+                	encryptLetterIndex = alphabetLetters.indexOf(textForEncodingLetters[i]) + key;
+            		if (encryptLetterIndex > alphabetLetters.size())
+                        encryptLetterIndex = encryptLetterIndex - alphabetLetters.size();
+            		else if (encryptLetterIndex < 0)
+            			encryptLetterIndex = encryptLetterIndex + alphabetLetters.size();
+                }           	
                 encryptedText = encryptedText + alphabetLetters.get(encryptLetterIndex);
-            }
-        }
+              }
         return encryptedText;
     }
 }
