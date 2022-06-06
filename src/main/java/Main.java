bffebe7374e9b985f99f4af44557a2fcad9be122
Vladimir Vujin <92719153+Vladimir-Vujin@users.noreply.github.com>
2022-06-06 13:08:00 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-
+	
     public static void main(String[] args) {
-
+    	
         Scanner scanner = new Scanner(System.in);
+        // INPUT TEXT
         System.out.print("Enter some text in capital letters (only letters A-Z): ");
         String inputTextForEncode = scanner.nextLine();
 
@@ -16,16 +17,77 @@ public class Main {
 
         // Second check if input text contains only letters of alphabet
         checkInputText(inputTextForEncode);
+        
+        // INPUT KEY
+        System.out.print("Enter key for encryption (only whole numbers): ");
+        String inputKeyForEncode = scanner.nextLine();
+        
+        // Clear empty spaces from beginning and end of input text, and format empty spaces to one space
+        inputKeyForEncode = formatInputText(inputKeyForEncode);
+        
+        // First check if input key is empty
+        checkInputContainsAnyNumbers(inputKeyForEncode);
+        
+        // Second check if input key contains only number
+        checkInputKey(inputKeyForEncode);
+        		
 
         System.out.println("Text to encrypt (formatted text): " + inputTextForEncode);
+        System.out.println("Key for encrypt: " + inputKeyForEncode);
 
-        CaesarCipherClass cipherTekst = new CaesarCipherClass(inputTextForEncode);
-        System.out.print("Encrypted text (Caesar Cipher): ");
-        System.err.println(cipherTekst.encodeText());
+        CaesarCipherClass cipherTekst = new CaesarCipherClass(inputTextForEncode, inputKeyForEncode);
+        System.out.print("Encrypted text (Caesar Cipher): " + cipherTekst.encodeText());
+        
+        scanner.close();
+    }
+    
+    /**
+     * Method check input String if contain only alphabetic letters A-Z
+     *
+     * @param text type String
+     */
+    public static void checkInputKey(String key) {
+    	String keyP = "";
+    	
+        key = key.replaceAll("\\s", "");
+        
+        if (key.contains("-")) {
+        	keyP = key.substring(1, key.length());
+        }
+   
+        if (!keyP.matches("[0-9]*")) {
+            System.err.println("Incorrect input - not allowed characters in key! Try again! Program ends!");
+            System.exit(0);
+        }
+    }
+    
+    /**
+     * Method check input String is non blank text
+     *
+     * @param text type String
+     */
+    public static void checkInputContainsAnyNumbers(String key) {
+        if (key.isBlank()) {
+            System.err.println("Incorrect input - blank key! Try again! Program ends!");
+            System.exit(0);
+        }
+    }
+    
+    /**
+     * Method format empty spaces in String, clear from beginning to end, and format multiple spaces to one space
+     *
+     * @param key type String
+     * @return String key
+     */
+    public static String formatInputKey(String key) {
+        key = key.replaceAll("( )+", " ");
+        key = key.trim();
+
+        return key;
     }
 
     /**
-     * Method format empty spaces in String, clear from beginning, end and format multiple spaces o one space
+     * Method format empty spaces in String, clear from beginning to end, and format multiple spaces to one space
      *
      * @param text type String
      * @return String text
